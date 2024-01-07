563351a3b9459d0984f96170463bf687f53f0261
SiddheshKanawade <siddhesh.kanawade@iitgn.ac.in>
2024-01-07 20:47:35 +0530

    add: logger

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { config } from "dotenv";
+import morgan from "morgan";
 
 // Load env variables
 config();
@@ -8,6 +9,7 @@ const app = express();
 
 // middlewares
 app.use(express.json()); // specify req and res params are json format
+app.use(morgan("dev")); // log requests to console in dev mode
 
 // routes
 export default app;
